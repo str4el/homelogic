e072d6a6a9c85b4295bf977de7438fc3e030191f
Stephan Reinhard <Stephan-Reinhard@gmx.de>
2014-12-13 17:42:18 +0100

    Grundgerüst für neues menu geschaffen

--- Makefile
+++ Makefile
@@ -1,11 +1,24 @@
-SUBDIRS = firmware libhl test
+
+ifneq ($(MAKECMDGOALS),menuconfig)
+
+include .config
+
+endif # MAKECMDGOALS!=menuconfig
+
+
+SUBDIRS-$(BUILD_FIRMWARE) += firmware
+SUBDIRS-$(BUILD_LIBRARY) += libhl
+SUBDIRS-$(BUILD_TEST) += test
+
+SUBDIRS = $(SUBDIRS-y)
+
 SUBCLEAN = $(addsuffix .clean,$(SUBDIRS))
 
 .PHONY: subdirs $(SUBDIRS) clean $(SUBCLEAN)
 
 all: subdirs
 
-subdirs: $(SUBDIRS)
+subdirs: .config $(SUBDIRS)
 
 clean: $(SUBCLEAN)
 
@@ -14,7 +27,22 @@ $(SUBDIRS):
 
 $(SUBCLEAN): %.clean:
 	$(MAKE) -C $* clean
-	
+
 
 test: libhl
+
+
+CONFIG_SHELL := $(shell if [ x"`uname`" = x"Darwin" ] && [ -x /opt/local/bin/bash ] ; then echo /opt/local/bin/bash; \
+          elif [ x"`uname`" = x"Darwin" ] && [ -x /usr/local/bin/bash ] ; then echo /usr/local/bin/bash; \
+          elif [ x"`uname`" = x"FreeBSD" ]; then echo /usr/local/bin/bash; \
+          elif [ -x "$$BASH" ]; then echo $$BASH; \
+          elif [ -x /bin/bash ]; then echo /bin/bash; \
+          elif [ -x /usr/local/bin/bash ]; then echo /usr/local/bin/bash; \
+          else echo sh; fi)
+
+
+menuconfig:
+	$(MAKE) -C scripts/lxdialog all
+	$(CONFIG_SHELL) scripts/Menuconfig config.in
+	test -e .config
 
---
+++ defaults.mk
@@ -0,0 +1,85 @@
+F_CPU ?= a8000000UL
+MCU ?= atmega32
+
+CP = cp
+RM = rm -f
+AVRDUDE = avrdude
+AVRDUDE_BAUDRATE = 115200
+AWK = gawk
+
+### use GNU m4 and GNU sed on FreeBSD
+ifeq ($(shell uname),FreeBSD)
+M4 = gm4
+SED = gsed
+else
+M4 = m4
+### use GNU sed from macports instead of BSD sed on MacOS X 
+SED = $(shell [ x"`uname`" = x"Darwin" ] && echo g)sed
+endif 
+
+
+CFLAGS += -std=gnu99 -Wall -Wstrict-prototypes 
+LFLAGS += -lm
+
+ifeq ($(PORPOSE),mcu)
+CC=avr-gcc
+AR=avr-ar
+OBJCOPY = avr-objcopy
+OBJDUMP = avr-objdump
+AS = avr-as
+SIZE = avr-size
+STRIP = avr-strip
+CFLAGS += -gstabs -Os -mmcu=$(MCU)
+LFLAGS += -mmcu=$(MCU)
+
+# reduce memory usage
+CFLAGS += -funsigned-char
+CFLAGS += -funsigned-bitfields
+CFLAGS += -fpack-struct
+CFLAGS += -fshort-enums
+CFLAGS += -mcall-prologues
+
+ifeq ($(BOOTLOADER_SUPPORT),y)
+LFLAGS += -Wl,--section-start=.text=$(BOOTLOADER_START_ADDRESS)
+endif
+
+else
+CC=gcc
+AR=ar
+OBJCOPY = objcopy
+OBJDUMP = objdump
+AS = as
+SIZE = size
+STRIP = strip
+
+ifeq ($(PURPOSE),host_lib)
+CFLAGS += -fpic -fvisibility=hidden
+LFLAGS += -Wl,-O1 -shared 
+endif
+
+ifeq ($(DEBUG),y)
+CFLAGS += -ggdb -O0
+endif
+
+endif #($(PORPOSE),mcu)
+
+
+
+# remove all unused code and data during linking
+CFLAGS += -fdata-sections -ffunction-sections
+LFLAGS += -Wl,--gc-sections,--relax
+
+
+%.s: %.c
+	$(CC) -o $@ $(CFLAGS) -S $<
+
+%.E: %.c
+	$(CC) -o $@ $(CFLAGS) -C -E -dD $<
+
+%.o: %.S
+	$(CC) -o $@ $(CFLAGS) $(ASFLAGS) -c $<
+
+%.o: %.c
+	$(CC) -o $@ -c $(CFLAGS) $<
+
+
