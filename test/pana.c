ee1514bcac6ffbba9b108556a10d3e6736568c54
Stephan Reinhard <stephan-reinhard@gmx.de>
2015-07-23 14:42:59 +0200

    Programmanalysierer geschrieben

---
+++ test/pana.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../libhl/homelogic.h"
+#include "../common/pattern.h"
+
+
+const struct {
+        uint8_t code;
+        char *name;
+} opcodes[] = {
+#define OPCODE(x, NAME, NUM, y, z) { NUM, NAME },
+#include "../common/opcodes.def"
+#undef OPCODE
+};
+
+
+
+
+void usage(void)
+{
+        fprintf(stderr, "pana hexfile [cpu]\n");
+        exit(1);
+}
+
+
+
+void analyze_address(const struct map_address_s *ma)
+{
+        switch(ma->ma_mem_adr & 0xE0) {
+        case as_memory:
+                printf("%%MW:%hhu.%hhu\n", ma->ma_device_adr, (ma->ma_mem_adr & 0x1F) << 1);
+                break;
+        case as_input:
+                printf("%%IW:%hhu.%hhu\n", ma->ma_device_adr, (ma->ma_mem_adr & 0x1F) << 1);
+                break;
+        case as_output:
+                printf("%%OW:%hhu.%hhu\n", ma->ma_device_adr, (ma->ma_mem_adr & 0x1F) << 1);
+                break;
+        case as_timer:
+                printf("%%T:%hhu.%hhu\n", ma->ma_device_adr, ma->ma_mem_adr & 0x1F);
+                break;
+        case as_counter:
+                printf("%%C:%hhu.%hhu\n", ma->ma_device_adr, ma->ma_mem_adr & 0x1F);
+                break;
+        default:
+                printf("Invalid address %02X %02X\n", ma->ma_device_adr, ma->ma_mem_adr);
+                break;
+        }
+}
+
+
+
+
+char get_memtype(const struct command_s *cmd)
+{
+        switch(cmd->c_address.aa_spec & 0xE0) {
+        case as_input:   return 'I';
+        case as_output:  return 'O';
+        case as_memory:  return 'M';
+        case as_timer:   return 'T';
+        case as_counter: return 'C';
+        }
+        return '?';
+}
+
+
+
+void analyze_command(const struct command_s *cmd)
+{
+        const char *name = "Ivalid";
+
+        for (int i = 0; i < sizeof(opcodes) / sizeof(*opcodes); i++) {
+                if (opcodes[i].code == cmd->c_opcode) {
+                        name = opcodes[i].name;
+                        break;
+                }
+        }
+
+        uint8_t dev = cmd->c_address.aa_device;
+        uint8_t byte = cmd->c_address.aa_byte;
+
+        switch(cmd->c_address.aa_spec & 0xE0) {
+        case as_input:
+        case as_output:
+        case as_memory:
+                switch(cmd->c_address.aa_spec & 0x1F) {
+                case as_byte:
+                        printf("%s\t%%%cB:%hhu.%hhu\n", name, get_memtype(cmd), dev, byte);
+                        break;
+                case as_word:
+                        printf("%s\t%%%cW:%hhu.%hhu\n", name, get_memtype(cmd), dev, byte);
+                        break;
+                default:
+                        printf("%s\t%%%cX:%hhu.%hhu.%hhu\n", name, get_memtype(cmd), dev, byte, cmd->c_address.aa_spec & 0x07);
+                }
+                break;
+
+        case as_timer:
+        case as_counter:
+                printf("%s\t%%%c:%hhu.%hhu\n", name, get_memtype(cmd), dev, byte);
+                break;
+
+        case as_constant:
+                printf("%s\t0x%04X\n", name, (uint16_t) dev << 8 | byte);
+                break;
+
+        default:
+                printf("%s\n", name);
+
+        }
+
+}
+
+
+
+
+int analyze_data(const hl_device_data_t *data)
+{
+        if (!data->dd_program_size) return 0;
+        printf("Size: %hu\n", data->dd_program_size);
+
+        if (data->dd_program_size < sizeof(struct program_header_s)) {
+                printf("Program to small!\n");
+                return -1;
+        }
+
+        const struct program_header_s *ph = (const struct program_header_s *) data->dd_program_memory;
+
+        printf("Address map size: %hu\n", ph->ph_address_map_size);
+        printf("Program size: %hu\n\n", ph->ph_program_size);
+
+        uint16_t size = sizeof(struct program_header_s);
+        size += ph->ph_address_map_size * sizeof(struct map_address_s);
+        size += ph->ph_program_size * sizeof(struct command_s);
+
+        if (data->dd_program_size != size) {
+                printf("Datasize doesn't match Headerdata!\n");
+                return 1;
+        }
+
+
+        printf("Address map:\n");
+        for (int i = 0; i < ph->ph_address_map_size; i++) {
+                const struct map_address_s *ma = (void *) data->dd_program_memory
+                                + ph->ph_address_map_offset + i * sizeof(*ma);
+                analyze_address(ma);
+        }
+        printf("\n");
+
+        printf("Commands:\n");
+        for (int i = 0; i < ph->ph_program_size; i++) {
+                const struct command_s *cmd = (void *) data->dd_program_memory
+                                + ph->ph_program_offset + i * sizeof(*cmd);
+                analyze_command(cmd);
+        }
+
+
+        return 0;
+}
+
+
+
+int main (int argc, char *argv[])
+{
+        int cpu = -1;
+        char *filename;
+
+        if (argc < 2) {
+                usage();
+        }
+
+        filename = argv[1];
+
+        if (argc > 2) {
+                if (1 != sscanf(argv[2], "%i", &cpu)) {
+                       usage();
+                }
+        }
+
+
+        hlc_t *hlc = hl_compiler_init();
+        if (!hlc) {
+                fprintf(stderr, "Error: Init hlc\n");
+                exit(1);
+        }
+
+        FILE *in = fopen(filename, "r");
+        if (!in) {
+                fprintf(stderr, "Error: fopen\n");
+                hl_compiler_destroy(hlc);
+                exit(1);
+        }
+
+        if (hl_read_intel_hex(hlc, in)) {
+                fprintf(stderr, "Error: read_hex\n");
+                hl_compiler_destroy(hlc);
+                fclose(in);
+                exit(1);
+        }
+
+        fclose(in);
+
+        if (cpu == -1) {
+                for (int i = 0; i < sizeof(hlc->d_device) / sizeof(*hlc->d_device); i++) {
+                        if (hlc->d_device[i].dd_program_size) {
+                                printf("Device %i:\n", i);
+                                analyze_data(&hlc->d_device[i]);
+                                printf("\n");
+                        }
+                }
+        } else {
+                if (cpu >= 0 && cpu < sizeof(hlc->d_device) / sizeof(*hlc->d_device)) {
+                        analyze_data(&hlc->d_device[cpu]);
+                }
+        }
+
+        hl_compiler_destroy(hlc);
+        return 0;
+
+}
